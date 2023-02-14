8de76071e69fbdb2b3b4045be2859a7cf3b06f1e
oniixu <125409306+oniixu@users.noreply.github.com>
2023-02-14 22:55:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const BTCMiner = require('bitcoin-miner');
 // View this block in Block Explorer:  https://insight.bitpay.com/block/00000000000000000020cf2bdc6563fb25c424af588d5fb7223461e72715e4a9
 // Get it in JSON format: https://insight.bitpay.com/api/block/00000000000000000020cf2bdc6563fb25c424af588d5fb7223461e72715e4a9
 const block = {
-    version: 536870912,
+    version: 0x20000000
+Nonce,
     previousblockhash: '00000000000000000061abcd4f51d81ddba5498cff67fed44b287de0990b7266',
     merkleroot: '871148c57dad60c0cde483233b099daa3e6492a91c13b337a5413a4c4f842978',
     time: 1515252561,
@@ -14,7 +15,7 @@ let hash;
 let found = false;
 
 
-let nonce = 45281998 // initial nonce
+let nonce = 0xeae0d571 // initial nonce
 while (!found) { 
     hash = miner.getHash(nonce);
     console.log(hash.toString('hex'));
